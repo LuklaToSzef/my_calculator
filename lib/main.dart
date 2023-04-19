8192c665a628a16d1f5b1d57362299287fecf690
marcin <marcin.hinz6@wp.pl>
2023-04-19 12:16:37 +0200

    d

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Calculator extends StatelessWidget {
             backgroundColor: Colors.black,
             minimumSize: const Size(90, 90),
             maximumSize: const Size(90, 90),
-              side: const BorderSide(color: Colors.blueGrey, width: 1.5),
+              side: const BorderSide(color: Colors.white, width: 2),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(360),
             ),
@@ -43,9 +43,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void calc(){
 
+  int temp = 0;
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+  String result = "0";
+  String numbers = "";
+  void calc(String buttonText) {
+    setState(() {
+      if (buttonText == "C") {
+        result = "0";
+        num1 = 0.0;
+        num2 = 0.0;
+        numbers ="";
+        operand = "";
+      } else if (buttonText == "+" || buttonText == "-" || buttonText == "×" || buttonText == "÷") {
+        num1 = double.parse(result);
+        operand = buttonText;
+        numbers += buttonText;
+        result = "0";
+      } else if (buttonText == ",") {
+        if (!result.contains(",")) {
+          result += ".";
+          numbers += buttonText;
+        }
+      } else if (buttonText == "=") {
+        num2 = double.parse(result);
+        if (operand == "+") {
+          if ((num1 + num2) % 1 == 0) {
+            temp = (num1 + num2).toInt();
+            result = temp.toString();
+          }
+          else {
+            result = (num1 + num2).toString();
+          }
+        }
+        if (operand == "-") {
+          if ((num1 - num2) % 1 == 0) {
+            temp = (num1 - num2).toInt();
+            result = temp.toString();
+          }
+          else {
+            result = (num1 - num2).toString();
+          }
+        }
+
+        if (operand == "×") {
+          if ((num1 * num2) % 1 == 0) {
+            temp = (num1 * num2).toInt();
+            result = temp.toString();
+          }
+          else {
+            result = (num1 * num2).toString();
+          }
+        }
+        //if (operand == "÷") {
+          //if (num2==0){
+           // result = "You can't divide by 0";
+         // }
+          //else {
+           // result = (num1 / num2).toString();
+          //}
+       // }
+      if (operand == "÷") {
+        if (num2 == 0) {
+          result = "You can't divide by 0";
+        }
+        else {
+          if ((num1 / num2) % 1 == 0) {
+      temp = (num1 ~/ num2);
+      result = temp.toString();
+      }
+      else {
+      result = (num1 / num2).toString();
+      }
+        }
+      }
+        num1 = 0.0;
+        num2 = 0.0;
+        operand = "";
+      } else {
+        if (result == "0") {
+          result = buttonText;
+          numbers += buttonText;
+        } else {
+          result += buttonText;
+          numbers += buttonText;
+        }
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
 
@@ -59,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         centerTitle: true,
       ),
-backgroundColor: Colors.blueGrey,
+backgroundColor: Colors.black,
       body: Center(
 
         child: Column (
@@ -67,65 +156,88 @@ backgroundColor: Colors.blueGrey,
           children: <Widget>[
             Container(
               width: 380,
-              height: 240,
+              height: 220,
               padding: const EdgeInsets.all(30),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.white, width: 2),
+                borderRadius: BorderRadius.circular(25),
               ),
-              child: const Text("result",style: TextStyle(fontSize: 40)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(numbers, style: const TextStyle(fontSize: 30, color: Colors.grey)),
+                  Text(result, style: const TextStyle(fontSize: 40, color: Colors.white)),
+                ],
+              ),
             ),
-
-
+            const SizedBox(height: 15),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
 
                 //button C ÷ × =
-                OutlinedButton(onPressed: calc, child:const Text("C",style: TextStyle(fontSize: 40, color: Colors.redAccent))),
-                OutlinedButton(onPressed: calc, child: const Text("÷",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("×",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child:  const Text("=",style: TextStyle(fontSize: 40, color: Colors.greenAccent)))
+                OutlinedButton(onPressed: () => calc('C'), child: const Text("C", style: TextStyle(fontSize: 40, color: Colors.redAccent))),
+                OutlinedButton(onPressed: () => calc('÷'), child: const Text("÷", style: TextStyle(fontSize: 40, color: Colors.lightBlueAccent))),
+                OutlinedButton(onPressed: () => calc('×'), child: const Text("×", style: TextStyle(fontSize: 40, color: Colors.lightBlueAccent))),
+                OutlinedButton(onPressed: () => calc('='), child: const Text("=", style: TextStyle(fontSize: 40, color: Colors.greenAccent))),
               ],
             ),
+            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children:  <Widget>[
                 //button 7 8 9 -
-                OutlinedButton(onPressed: calc, child: const Text("7",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("8",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("9",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child:  const Text("-",style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('7'), child: const Text("7", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('8'), child: const Text("8", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('9'), child: const Text("9", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('-'), child: const Text("-", style: TextStyle(fontSize: 40, color: Colors.lightBlueAccent))),
 
               ],
             ),
+            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 //button 4 5 6 +
-                OutlinedButton(onPressed: calc, child: const Text("4",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("5",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("6",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("+",style: TextStyle(fontSize: 40, color: Colors.white)))
+                OutlinedButton(onPressed: () => calc('4'), child: const Text("4", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('5'), child: const Text("5", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('6'), child: const Text("6", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('+'), child: const Text("+", style: TextStyle(fontSize: 40, color: Colors.lightBlueAccent))),
               ],
             ),
+            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 //button 1 2 3 ,
-                OutlinedButton(onPressed: calc, child: const Text("1",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("2",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text("3",style: TextStyle(fontSize: 40, color: Colors.white))),
-                OutlinedButton(onPressed: calc, child: const Text(",",style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('1'), child: const Text("1", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('2'), child: const Text("2", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc('3'), child: const Text("3", style: TextStyle(fontSize: 40, color: Colors.white))),
+                OutlinedButton(onPressed: () => calc(','), child: const Text(",", style: TextStyle(fontSize: 40, color: Colors.lightBlueAccent))),
 
 
               ],
             ),
+            const SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children:  <Widget>[
                 //button 0 (in the center)
-                OutlinedButton(onPressed: calc, child: const Text("0",style: TextStyle(fontSize: 40, color: Colors.white)),),
+                //OutlinedButton(onPressed: () => calc('0'), child: const Text("0", style: TextStyle(fontSize: 40, color: Colors.white))),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black,
+                    minimumSize: const Size(380, 90),
+                    maximumSize: const Size(380, 90),
+                      side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(360)
+                    )
+                  ),
+                  onPressed: () => calc('0'), child: const Text('0', style: TextStyle(fontSize: 40, color: Colors.white)),
+                )
 
               ],
             ),
